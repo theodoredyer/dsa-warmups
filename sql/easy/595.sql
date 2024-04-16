62ac66568693d60c6cf3ee07a24f202b1b4d6843
theodoredyer <theodore.austin.dyer@gmail.com>
2024-04-16 09:49:52 -0700

    Update 595.sql

--- sql/easy/595.sql
+++ sql/easy/595.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select World.name, World.population, World.area
 from World
-where World.area > 3000000 or World.population > 25000000
+where World.area >= 3000000 or World.population >= 25000000
