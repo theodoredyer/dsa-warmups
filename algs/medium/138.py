7962cfa15737eb9f81402f596f1d4fb29a8679c6
theodoredyer <theodore.austin.dyer@gmail.com>
2024-05-22 21:46:24 -0700

    5/22 warmups

---
+++ algs/medium/138.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        firstpass_ptr = head
+        secondpass_ptr = head
+        
+        old_to_new = {None: None}
+
+        while firstpass_ptr:
+            newnode = Node(firstpass_ptr.val, None, None)
+            old_to_new[firstpass_ptr] = newnode
+            firstpass_ptr = firstpass_ptr.next
+
+        while secondpass_ptr:
+            newnode = old_to_new[secondpass_ptr]
+            newnode.next = old_to_new[secondpass_ptr.next]
+            newnode.random = old_to_new[secondpass_ptr.random]
+            
+            secondpass_ptr = secondpass_ptr.next
+
+        return old_to_new[head]
---
+++ algs/medium/143.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return 
+
+        nodelist = []
+
+        cur = head
+        dummy = head
+
+        while cur:
+            nodelist.append(cur)
+            cur = cur.next
+        
+        lptr = 0
+        rptr = len(nodelist) - 1
+
+        while lptr < rptr:
+            nodelist[lptr].next = nodelist[rptr]
+            lptr += 1
+
+            if lptr == rptr:
+                break
+
+            nodelist[rptr].next = nodelist[lptr]
+            rptr -= 1
+
+        nodelist[lptr].next = None
+        
