4ff08a5fdeb9813dc8cf8eab162aa33739b0a9e7
theodoredyer <theodore.austin.dyer@gmail.com>
2024-06-08 14:32:10 -0700

    start of 6/8

--- algs/medium/46.py
+++ algs/medium/46.py
@@ -1,21 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        res = []
 
-        # Base
         if len(nums) == 1:
-            return [nums[:]]
+            return [nums.copy()]
 
         for i in range(len(nums)):
-
-            n = nums.pop(0)
-
+            head = nums.pop(0)
             perms = self.permute(nums)
-
             for perm in perms:
-                perm.append(n)
+                perm.append(head)
+            res.extend(perms)
+            nums.append(head)
+        return res
 
-            result.extend(perms)
-            nums.append(n)
 
-        return result
+"""
+Backtracking problem - need to identify the pattern that whenever we are looking
+for perms, we want to isolate off the head and then compute all of the permutations
+of the remaining numbers, and then add the head back on after computing all of the perms
+"""
