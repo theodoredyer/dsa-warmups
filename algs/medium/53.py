f6f0a06e9566f33dfa3d18975a6dbdcbdb1b508d
theodoredyer <theodore.austin.dyer@gmail.com>
2024-06-09 16:59:18 -0700

    6/9p4

--- algs/medium/53.py
+++ algs/medium/53.py
@@ -15,3 +15,15 @@ def maxSubArray(nums):
 
 print(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
 print(maxSubArray([5, 4, -1, 7, 8]))
+
+
+"""
+To calculate max subarray, just traverse forwards seeing if the current
+accumulation is greater than the max, if we ever arrive at a point where
+we are at accumulation < 0, just reset to 0 because we're allowed to ignore
+sections of the array that don't contribute positively. 
+
+after resetting to 0, continue to check to see if we can find a greater max,
+checking at each iteration if we're at a new max. 
+
+"""
