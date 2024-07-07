5ce2939f38d648a678d74722bd9e969902cb1e86
theodoredyer <tdyer@ucsc.edu>
2024-07-07 15:30:30 -0700

    Update 238.py

--- algs/medium/238.py
+++ algs/medium/238.py
@@ -1,36 +1,38 @@
-def productExceptSelf(nums):
-        
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefixes = [1] * len(nums)
+        postfixes = [1] * len(nums)
 
-    # [1, 2, 3, 4]
-    # [24, 12, 8, 6]
+        # calc prefixes
+        i = 1
+        while i < len(prefixes):
+            prefixes[i] = prefixes[i-1] * nums[i-1]
+            i += 1
+        print(prefixes)
 
-    result = [None] * len(nums)
+        # calc postfixes
+        i = len(postfixes) - 2
+        while i >= 0:
+            postfixes[i] = nums[i+1] * postfixes[i+1]
+            i -= 1
 
-    # first pass
-    # generate prefix values
-    for i in range(len(nums)):
-        if(i == 0):
-            result[i] = 1
-        elif(i == 1):
-            result[i] = nums[0]
-        else:
-            result[i] = result[i-1] * nums[i-1]
+        return [prefixes[i] * postfixes[i] for i in range(len(nums))]
+    
 
-    # second pass
-    # generate postfix values
-    i = len(nums) - 1
-    postfix_factor = None
-    for iterations in range(len(nums)):
+"""
+Simple problem just using prefix and postfix multiplication, 
 
-        if i == len(nums) - 1:
-            result[i] *= 1
-            postfix_factor = nums[i]
-        else:
-            result[i] *= postfix_factor
-            postfix_factor *= nums[i]
+Just remember that the prefixes should look like
+i = 1
+while i < len(nums):
+    prefix[i] = nums[i-1] * prefix[i-1]
 
-        i -= 1
+and postfix similarly is 
+i = len(nums) - 1
+while i >= 0:
+    postfix[i] = nums[i+1] * postfix[i+1]
 
-    return result
+At no point in the definition for postfix or prefix of i is the actual 
+index of i involved. 
 
-productExceptSelf([1, 2, 3, 4])
+"""
