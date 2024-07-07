dd3b75f211a7bc28d754fa2eaa275ebf16d696cb
theodoredyer <tdyer@ucsc.edu>
2024-07-07 15:08:22 -0700

    Update 347.py

--- algs/medium/347.py
+++ algs/medium/347.py
@@ -1,24 +1,37 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        seen = {}
-        frequency = [[] for i in range(len(nums) + 1)]
+        
+        occurrances = {}
 
         for n in nums:
-            if n in seen:
-                seen[n] += 1
-            else:
-                seen[n] = 1
-        
-        for n, c in seen.items():
-            frequency[c].append(n)
+            occurrances[n] = occurrances.get(n, 0) + 1
 
-        result = []
+        frequencies = [[] for i in range(len(nums) + 1)]
 
-        for i in range(len(frequency) - 1, 0, -1):
-            for n in frequency[i]:
-                result.append(n)
-                if(len(result) == k):
-                    return result
+        for key, value in occurrances.items():
+            frequencies[value].append(key)
+
+        print(frequencies)
+
+        k_added = 0
+        ptr = len(frequencies) - 1
+        res = []
+        while k_added < k:
+            for item in frequencies[ptr]:
+                res.append(item)
+                k_added += 1
+                if k_added == k:
+                    return res
+            ptr -= 1
 
 
-        
+"""
+Set up a dictionary to track the occurrances of each number, and then 
+set up a list of lists, where the index of this list represents the frequency
+of the number count, and the value of this list is going to be another list containing
+all of the characters that have that frequency
+
+We populate this by looking through our dictionary, and for each character we see what the frequency was
+and then populate the frequency array at that count to contain that character. 
+
+"""
